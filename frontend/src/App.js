cf0399e52437f6f3715914da55b5a786e954d0df
Bhargav Kumbham <bhargavkumbham@192.168.1.10>
2025-08-31 20:34:28 +0530

    commited changes successfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Body from './components/Body'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RegForm from './components/RegisterForm';
 function App() {
   return (
-    <div><Navbar /><Router>
-<Routes>
-        <Route path="/register" element={<RegForm />} />
-        <Route path="/login" element={<RegForm />} />
- </Routes>
-    </Router><Body /></div>
+    <div><Navbar /><Body /></div>
   )
 }
 
